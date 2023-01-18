0d23a6aa58752c6a4d8b0ca76f29f5c7082983b0
annapng <64225221+annapng@users.noreply.github.com>
2023-01-18 14:55:51 -0700

    Finally fixing folder issues!!
    Had problems with the cloned folder not appearing on GitHub

---
+++ New folder (2)/Develop/routes/APIroutes.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const router = require('express').Router();
+const {v4: uuidv4} = require('uuid');
+const {notes} = require('../db/db.json');
+const {createNote, findID, editNote, removeNote} = require('../notes.js');
+
+uuidv4();
+
+
+router.get('../notes.js', (req, res) => {
+
+    res.json(notes);
+
+});
+
+router.post('../notes.js', (req, res) => {
+    
+    if(!req.body.id) {
+        req.body.id = uuidv4();
+        createNote(req.body, notes);
+    } else {
+        editNote(req.body, notes);
+    }
+
+    res.json(req.body);
+
+});
+
+
+router.delete('../notes.js/:id', (req, res) => {
+
+    const note = findID(req.params.id, notes);
+
+    removeNote(note, notes);
+    res.json();
+
+});
+
+
+
+module.exports = router;

52f1f1e5390188069a81a08c57c15dadb15f72d1
annapng <64225221+annapng@users.noreply.github.com>
2023-01-22 15:43:28 -0700

    Update

---
+++ miniature-eureka/Develop/routes/HTMLroutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+
+
+router.get('/notes', (req, res) => {
+    res.sendFile(__dirname + '../public/notes.html');
+  });
+  
+router.get('/', (req, res) => {
+    res.sendFile(__dirname + '../public/index.html');
+  });
+
+
+
+module.exports = router;
+  
---
+++ miniature-eureka/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
+const apiRoutes = require('/Develop/routes/apiRoutes');
+const htmlRoutes = require('/Develop/routes/htmlRoutes');
+
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.use(express.static('public'));
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
+//
